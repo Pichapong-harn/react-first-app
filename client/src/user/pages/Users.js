c1426898d979e4dca605b282296cd604084cda36
penguinkanong <penguinkanong@github.com>
2020-05-15 21:57:01 +0700

    add place

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -3,7 +3,15 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USER = [{ id: "u1", name: "penguin kanong", image: "", places: 3 }];
+  const USER = [
+    {
+      id: "u1",
+      name: "penguin kanong",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 3,
+    },
+  ];
   return <UsersList items={USER} />;
 };
 
