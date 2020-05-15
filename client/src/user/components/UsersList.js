aa1a943ea06993cb5e2d4fdc46823a97cb8c9ed1
penguinkanong <i__wayne@hotmial.com>
2020-05-15 17:17:18 +0700

    add nav

--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import "./UsersList.css";
-import UserItem from "./UserItem";
+import './UsersList.css';
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
-      <div className="center">
-        <h2>No user found.</h2>
+      <div className='center'>
+        <Card>
+          <h2>No user found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
-    <ul className="users-list">
+    <ul className='users-list'>
       {props.items.map((user) => (
         <UserItem
           key={user.id}
