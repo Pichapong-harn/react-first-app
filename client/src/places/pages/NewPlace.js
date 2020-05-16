7294c1ad3ee081b511bd405a5e685ae1c6c5e824
penguinkanong <penguinkanong@github.com>
2020-05-16 15:45:03 +0700

    validate form

--- client/src/places/pages/NewPlace.js
+++ client/src/places/pages/NewPlace.js
@@ -1,11 +1,19 @@
 import React from "react";
-import Input from '../../shared/components/FormElements/Input'
-import './NewPlace.css'
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className="place-form">
-    <Input element="input" type="text" label="title" />
-  </form>
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="title"
+        validator={[]}
+        errorText="Plese enter a valid title."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
